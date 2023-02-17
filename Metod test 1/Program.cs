929ec9d68c84feef9cb5a4499a4d83f82cfb5b98
Hugge25 <hibba04@hotmail.se>
2023-02-17 10:07:24 +0100

    Add project files.

---
+++ Metod test 1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace namn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            area();
+        }
+
+        static void area()
+        {
+            Console.Write("Skriv in rektanglens bredd: ");
+            string bredd = Console.ReadLine();
+            Console.Write("Skriv in rektanglens höjd: ");
+            string höjd = Console.ReadLine();
+
+            int bredd2 = int.Parse(bredd);
+            int höjd2 = int.Parse(höjd);
+
+            Console.WriteLine($"Din rektangel med bredd {bredd2} och höjd {höjd2} har arean {bredd2 * höjd2}");
+        }
+    }
+}
